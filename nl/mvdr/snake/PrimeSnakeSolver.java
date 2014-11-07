57cc4367a83a3643970ccfd1a506c4d0e92e400a
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-11-08 00:53:29 +0100

    Moved code into a package.

--- nl/mvdr/snake/PrimeSnakeSolver.java
+++ nl/mvdr/snake/PrimeSnakeSolver.java
@@ -1,3 +1,4 @@
+package nl.mvdr.snake;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
---
+++ nl/mvdr/snake/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package containing a solution to Java Magazine 05 2014's "Snake" Code Challenge.
+ * 
+ * @author Martijn van de Rijdt
+ */
+package nl.mvdr.snake;
