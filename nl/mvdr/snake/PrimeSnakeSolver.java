e2919d8e1b9a7688acd8d33910523a48c31bc812
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-11-08 00:56:03 +0100

    Removed unused imports.

--- nl/mvdr/snake/PrimeSnakeSolver.java
+++ nl/mvdr/snake/PrimeSnakeSolver.java
@@ -3,8 +3,6 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 
