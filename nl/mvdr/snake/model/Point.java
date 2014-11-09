f7e4818572406e771445665b2294639b2c6e4f62
Martijn van de Rijdt <mvdrijdt@planet.nl>
2014-11-09 13:59:02 +0100

    Revised equals, added hashCode.

--- nl/mvdr/snake/model/Point.java
+++ nl/mvdr/snake/model/Point.java
@@ -18,20 +18,6 @@ public class Point {
         this.y = y;
     }
 
-    @Override
-    public String toString() {
-        return "(" + x + "," + y + ")";
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof Point)) {
-            return false;
-        }
-        Point other = (Point) obj;
-        return other.x == x && other.y == y;
-    }
-
     public int getX() {
         return x;
     }
@@ -49,5 +35,35 @@ public class Point {
     public Point move(Direction direction) {
         return new Point (x + direction.getPoint().getX(), 
                 y + direction.getPoint().getY());
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + y;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Point other = (Point) obj;
+        if (x != other.x)
+            return false;
+        if (y != other.y)
+            return false;
+        return true;
+    }
+    
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
     }
 }
